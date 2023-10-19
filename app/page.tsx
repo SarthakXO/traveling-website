30ce9645181e96cd6f425a66fa493b85644b3f32
Sarthak <sarthakbhatt@live.com>
2023-10-19 15:15:32 +0530

    Navbar created and links added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-teal-500 font-bold">Hello world</h1>;
+  return <h1 className="text-teal-500 font-bold text-2xl">Hello world</h1>;
 }
